5975ac797a7ed74e06c4e8524785d6db105419a9
kennardconsulting <richard@kennardconsulting.com>
2017-02-22 17:29:09 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# aframe-reset-on-collision
+# A-Frame: reset-on-collision
+
+## What Is It?
+
+Resets an `a-entity` position to the 'last known good' position after any simple AABB collision.
+Will only work for axis-aligned entities (such as simple walls).
+
+## What's It Good For?
+
+If attached to a `camera`, will prevent walking through walls using WASD controls and/or physically
+moving in a room-scale environment. For the latter, the world will appear to 'push back' against the
+user.
+
+## What Isn't It Good For?
+
+Collision detection uses simple (but fast) AABB collision detection. So will only work for simple axis
+aligned primitives (such as `a-plane` or `a-box` used as walls). Also the positional reset is a
+bit janky, and it doesn't implement 'wall sliding' when using WASD controls.
+
+## Who Should Use It?
+
+Anyone looking for a quick and dirty solution to stopping people walking through walls in environments
+where they shouldn't really be going anywhere near the walls in the first place. For example, it's not
+good for a First Person Shooter where people try to hug the walls (because it doesn't implement 'wall
+sliding'). But it's fine for an art gallery viewing application.
+
+## How Do I Use It?
+
+Add to an `a-entity`. For example:
+
+    <a-entity camera reset-on-collision="wall: .wall"/>
---
+++ src/colliding-camera.js
@@ -0,0 +1,81 @@
+(function() {
+
+	var collideWithBoundingBox = new THREE.Box3();
+	
+	/**
+	 * Resets an entity's position to the 'last known good' position after any simple AABB
+	 * collision. Will only work for axis-aligned entities (such as walls).
+	 * <p>
+	 * Author: @kennardconsulting
+	 */
+	 
+	AFRAME.registerComponent( 'reset-on-collision', {
+		schema: {
+			// Selector to collide with
+			with: {default: '.wall'}
+		},
+	
+		init: function() {
+			// Choose a geometry size that is small enough to not get stuck often, but large enough to catch WASD movement
+			
+			this.el.setAttribute( 'geometry', 'width: 0.2; depth: 0.2; height: 0.2' );
+			 
+			this.mesh = this.el.getObject3D( 'mesh' );
+			this.boundingBox = new THREE.Box3();
+			this.collideWiths = this.el.sceneEl.querySelectorAll( this.data.with );
+		},
+	
+		/**
+		 * On every tick, check for collision. If found, reset to last known good position.
+		 */
+		 
+		tick: function () {
+	
+			this.boundingBox.setFromObject( this.mesh );
+			var thisMin = this.boundingBox.min;
+			var thisMax = this.boundingBox.max;
+	
+			var self = this;
+	
+			if ( intersects() ) {
+				this.el.setAttribute( 'position', this.lastKnownGoodPosition );
+			} else {
+				this.lastKnownGoodPosition = {
+					x: this.el.object3D.position.x,
+					y: this.el.object3D.position.y,
+					z: this.el.object3D.position.z
+				}
+			}
+	
+			/**
+			 * Tests each collideWiths for intersection.
+			 */
+			 
+			function intersects() {
+	
+				for ( var loop = 0; loop < self.collideWiths.length; loop++) {
+					var collideWith = self.collideWiths[loop];
+	
+					if ( intersect( collideWith )) {
+						return true;
+					}
+				}
+				return false;
+			}
+			
+			/**
+			 * Tests a single entity for intersection. Uses simple AABB collision detection.
+			 */
+			 
+			function intersect( el ) {
+			
+				collideWithBoundingBox.setFromObject( el.getObject3D( 'mesh' ));
+				var collideWithMin = collideWithBoundingBox.min;
+				var collideWithMax = collideWithBoundingBox.max;
+				return (thisMin.x <= collideWithMax.x && thisMax.x >= collideWithMin.x) &&
+					   (thisMin.y <= collideWithMax.y && thisMax.y >= collideWithMin.y) &&
+					   (thisMin.z <= collideWithMax.z && thisMax.z >= collideWithMin.z);
+			}
+		}
+	} );
+})();
